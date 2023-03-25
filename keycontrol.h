601c3b2687382207cf6c440b0550a6ff156ee817
zylll789 <110252006+zylll789@users.noreply.github.com>
2023-03-26 01:29:14 +0800

    Add files via upload

---
+++ keycontrol.h
@@ -0,0 +1,59 @@
+#include <REGX52.H>
+
+#ifndef _KEY_NUM
+#define _KEYNUM
+    #define K1  1
+    #define K2  2
+    #define K3  3
+    #define K4  4
+    #define K11 11
+    #define K12 12
+    #define K13 13
+    #define K14 14
+    #define K21 21
+    #define K22 22
+    #define K23 23
+    #define K24 24
+    #define K31 31
+    #define K32 32
+    #define K33 33
+    #define K34 34
+    #define K41 41
+    #define K42 42
+    #define K43 43
+    #define K44 44 
+#endif
+
+//get the key you pressed
+char checkKeyDown(){
+    P2_4 = P2_5 = P2_6 = P2_7 = 1;
+    if(P2_0 == 0) return K4;
+    if(P2_1 == 0) return K3;
+    if(P2_2 == 0) return K2;
+    if(P2_3 == 0) return K1;
+    P2_4 = 0;
+    if(P2_0 == 0) return K44;
+    if(P2_1 == 0) return K43;
+    if(P2_2 == 0) return K42;
+    if(P2_3 == 0) return K41;
+    P2_4 = 1;
+    P2_5 = 0;
+    if(P2_0 == 0) return K34;
+    if(P2_1 == 0) return K33;
+    if(P2_2 == 0) return K32;
+    if(P2_3 == 0) return K31;
+    P2_5 = 1;
+    P2_6 = 0;
+    if(P2_0 == 0) return K24;
+    if(P2_1 == 0) return K23;
+    if(P2_2 == 0) return K22;
+    if(P2_3 == 0) return K21;
+    P2_6 = 1;
+    P2_7 = 0;
+    if(P2_0 == 0) return K14;
+    if(P2_1 == 0) return K13;
+    if(P2_2 == 0) return K12;
+    if(P2_3 == 0) return K11;
+    P2_7 = 1;
+    return 0;
+}
--- segutils.h
+++ segutils.h
@@ -1,3 +1,33 @@
+#include <REGX52.H>
+
+#ifndef _num_to_hex
+#define _num_to_hex
+
+    #define ZERO  0xc0//0
+    #define ONE   0xf9//1
+    #define TWO   0xa4//2
+    #define THREE 0xb0//3
+    #define FOUR  0x99//4
+    #define FIVE  0x92//5
+    #define SIX   0x82//6
+    #define SEVEN 0xf8//7
+    #define EIGHT 0x80//8
+    #define NINE  0x90//9
+
+    #define ZERO_DOT  0x40//0.
+    #define ONE_DOT   0x79//1.
+    #define TWO_DOT   0x24//2.
+    #define THREE_DOT 0x30//3.
+    #define FOUR_DOT  0x19//4.
+    #define FIVE_DOT  0x12//5.
+    #define SIX_DOT   0x02//6.
+    #define SEVEN_DOT 0x78//7.
+    #define EIGHT_DOT 0x00//8.
+    #define NINE_DOT  0x10//9.
+
+#endif
+
+//num ^ a
 int pow(int num, int a){
 	int i = 0;
 	int ans = 1;
@@ -5,4 +35,24 @@ int pow(int num, int a){
 		ans *= num;
 	}
 	return ans;
+}
+
+//t ms
+void delay(unsigned int t){
+    int i,j;
+    for (i = 0; i < 120; i++){
+        for (j = 0; j < t; j++);
+    }
+}
+
+//num -> defined num like ONE TWO or ONE_DOT for example,0 <= num <= 9
+//pos -> the position where light lit,0~7 -> 10^pos 
+void showNum(unsigned int num, unsigned int pos){
+    P0 = num;
+    P1 = 0xff - pow(2, pos);
+}
+
+void clearNum(){
+	P0 = 0xff;
+	P1 = 0xff;
 }
